01732bc84765b11358f1d38dadc4f8a78d4de4ce
Poli <polioubinha@gmail.com>
2023-11-18 10:51:04 +0100

    Inicio proyecto

---
+++ app/src/main/java/com/pmdm/proyecto/MainActivity.java
@@ -0,0 +1,13 @@
+package com.pmdm.proyecto;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
